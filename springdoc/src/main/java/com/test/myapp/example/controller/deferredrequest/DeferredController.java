dd2e000d821c9caef77a49d646c05fac9a207056
biubiubiubiubiubiubiu <rshen3@jhu.edu>
2018-11-26 00:34:54 +0800

    Spring MVC 处理异步请求实例代码更新

--- springdoc/src/main/java/com/test/myapp/example/controller/deferredrequest/DeferredController.java
+++ springdoc/src/main/java/com/test/myapp/example/controller/deferredrequest/DeferredController.java
@@ -5,12 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ForkJoinPool;
 
 @Controller
 public class DeferredController {
-
     @Autowired
     DeferredService deferredService;
 
@@ -24,10 +25,27 @@ public class DeferredController {
 
     @RequestMapping(value = "non-blocking-request.do", method = RequestMethod.GET)
     public Callable<String> nonBlockingService() {
-        System.out.println("start blocking-request...");
+        System.out.println("start non-blocking-request...");
 
         Callable<String> result = deferredService::execute;
         System.out.println("result retrieved!");
+        return result;
+    }
+
+    @RequestMapping(value = "deffered-result.do", method = RequestMethod.GET)
+    public DeferredResult<String> deferredResultService() {
+        System.out.println("start deferredResult request...");
+        DeferredResult<String> result = new DeferredResult<>();
+        ForkJoinPool.commonPool().submit(() -> {
+            System.out.println("start deferred result handling");
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("finished deferred result handling");
+            result.setResult("hello");
+        });
+        System.out.println("request finished!");
         return result;
     }
 
--- springdoc/src/main/java/com/test/myapp/example/service/DeferredService.java
+++ springdoc/src/main/java/com/test/myapp/example/service/DeferredService.java
@@ -1,16 +1,11 @@
 package com.test.myapp.example.service;
 
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ScheduledExecutorService;
+
 
 @Service
 public class DeferredService extends TaskService{
-
     @Override
     public String execute() {
         try {
